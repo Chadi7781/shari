87ac0f6c657543d8752ad690fbad650cf81f5868
Chadi Troudi <troudishedy6@gmail.com>
2022-01-11 11:18:39 +0100

    add some changes to input cp

--- components/input/Input.js
+++ components/input/Input.js
@@ -2,7 +2,24 @@ import { XIcon ,PhotographIcon, EmojiHappyIcon, ChartBarIcon, CalendarIcon} from
 import {useState,useRef} from 'react';
 import 'emoji-mart/css/emoji-mart.css'
 import { Picker } from 'emoji-mart'
+
+import {signOut, useSession} from 'next-auth/react';
+import {
+    addDoc,
+    collection,
+    doc,
+    serverTimestamp,
+    updateDoc
+} from '@firebase/firestore';
+
+import {db,storage} from '../../firebase';
+import { uploadString,ref ,getDownloadURL} from 'firebase/storage';
 function Input() {
+
+
+    //USE SESSION FROM NEXT AUTH 
+    // const {data: session} = useSession();
+
 
     const [input, setInput] = useState("");
     const [selectedFile, setSelectedFile] =useState(null);
@@ -21,20 +38,56 @@ function Input() {
     }
 
 
-    const sendPost = () => {
+    const sendPost = async() => {
         if(loading) return;
         setLoading(true);
+
+        const docRef = await addDoc(collection(db,"posts"), {
+            // id: session.user.uid,
+            // username: session.user.name,
+            // userImg:  session.user.image,
+            // tag: session.user.tag,
+            text: input,
+            timestamp:   serverTimestamp(),
+
+        });
+
+        const imageRef = ref(storage,`posts/${docRef.id}/image`);
+
+        if(selectedFile) {
+            await uploadString(imageRef,selectedFile,"data_url").then(
+                async () => {
+                    const downloadURL = await getDownloadURL(imageRef);
+                    await  updateDoc(doc(db,"posts",docRef.id), {
+                        image: downloadURL
+                    });
+                }
+            )
+        }
+
+        setLoading(false);
+        setInput("");
+        setSelectedFile(null);
+        setShowEmojis(false);
 
 
         
     }
-    const addImageToPost = () => {
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if(e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
 
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result);
+        };
 
     }
     return (
         <div className={`border-b border-x-gray-300 p-3
                         flex space-x-3 overflow-y-scroll
+                        ${loading && "opacity-60"}
     
         `}>
             <img src="https://lh3.googleusercontent.com/fife/AAWUweUMC2zfpoqMHzmuRK92U5xT0VQGQHXD8YDisDjkJj4rXGGxCw_EzVrKLS32dPowO1wxdgPoluuz00SqVZVnTt-ppAd0GHJnprmMEyyQ-gB-Chy4JzLmB9-wATUHJf5jBFe_NLHTDLTBnbdtO1ayhVQHL6KfuQZFp7-RRo5wTXMdRmYQ4z2eJiZaRnsrsA7rJR1ZjhsBsmmQ4Q4IlasN3twkrEyqor_lY_ThNGCKBhEYCxfn0wZ1Rnu5wtos_xJUnQDP_CXnjIONIJKPWe7f6lzagf7U4HFypN6kh9SWs8EFaQ0yc7Q36tpTZa1rrZTbUlVnIZi580d7Ow1f40bXueLCYOQ0m6DzGHkTasXLFeuVIdWene5nby__OR_b6I9wO5KGtbYhrrCGHZjWH-xjs3QQSdUBKTm7-sDA6jXnJFpOYC7kn3F9FuI-dK-yFNKlwha9WgAZn2Cj_6K8b0GWRtrnhtjRH7hi7djBdHRclTafUPhdqryUJ4oBYwZz86q73L138jyLZnWqFwy_IdYnuxGYvVpcm8sMcnIILcRhVoH4z97xcyX3f2EmQmG7PcWNwsKu1EZ1T5pmt5Q_lpLURP_dcKtg8675kZIxpR60icFvEGhREiQb7jIAQTFUF1L1JO_QzsFzrrF4xCwmMv4tz9X-_dhY1Tpm-lSsxiXkUMXciAdUL00dNpjzanPJT6HuonyhWofAac69swjMgHQ4N8pkna-_3PPHtINdQ-hHcq2SUg=s83-c"
@@ -60,7 +113,7 @@ function Input() {
                          hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center
                          justify-center top-1 left-1 cursor-pointer
                          "
-                         onClick={()=>selectedFile(null)}
+                         onClick={()=>setSelectedFile(null)}
                          
                          
                          >
@@ -79,69 +132,74 @@ function Input() {
                          
                  </div>
 
-                 <div className="flex items-center justify-between pt-2.5">
-                     <div className="flex items-center">
-                         <div className="icon" onClick={()=>filePickerRef.current.click()}>
-                             <PhotographIcon className="h-[22px] 
-                             text-[#1d9bf0] "/>
 
-                             <input 
-                             type="file"
-                             hidden
-                             onChange={addImageToPost}
-                             ref={filePickerRef}/>
-                         </div>
+                 {!loading && (
+         <div className="flex items-center justify-between pt-2.5">
+         <div className="flex items-center">
+             <div className="icon" onClick={()=>filePickerRef.current.click()}>
+                 <PhotographIcon className="h-[22px] 
+                 text-[#1d9bf0] "/>
 
-                         <div className="icon rotate-90">
-                             <ChartBarIcon className="h-[22px] text-[#1d9bf0]"/>
-                             
-                         </div>
+                 <input 
+                 type="file"
+                 hidden
+                 onChange={addImageToPost}
+                 ref={filePickerRef}/>
+             </div>
 
-
-                         <div className="icon" 
-                         
-                         onClick={() => setShowEmojis(!showEmojis)}
-                         >
-                             <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]"/>
-                         </div>
-                         <div className="icon">
-                             <CalendarIcon className="h-[22px] text-[#1d9bf0]"/>
-                         </div>
-
-                         {showEmojis && (
-                            <Picker
-                             onSelect={addEmoji}
-                            style={{
-                                position: 'absolute',
-                                marginTop: "465px",
-                                marginLeft: -40,
-                                maxWidth: "300px",
-                                borderRadius: "20px",
-                            }}/>
+             <div className="icon rotate-90">
+                 <ChartBarIcon className="h-[22px] text-[#1d9bf0]"/>
+                 
+             </div>
 
 
-                         )}
-                            
+             <div className="icon" 
+             
+             onClick={() => setShowEmojis(!showEmojis)}
+             >
+                 <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]"/>
+             </div>
+             <div className="icon">
+                 <CalendarIcon className="h-[22px] text-[#1d9bf0]"/>
+             </div>
+
+             {showEmojis && (
+                <Picker
+                 onSelect={addEmoji}
+                style={{
+                    position: 'absolute',
+                    marginTop: "465px",
+                    marginLeft: -40,
+                    maxWidth: "300px",
+                    borderRadius: "20px",
+                }}/>
 
 
-                     </div>
+             )}
+                
 
-                  <button className="
-                     bg-[#1d9bf0] text-white 
-                     transition duration-300 ease-in-out
 
-                     
-                     rounded-full px-4 py-1.5
-                      font-bold shadow-md hover:bg-[#1a8cd8] 
-                      disabled:hover:bg-[#1d9bf0] disabled:opacity-50 
-                      disabled:cursor-default"
-                      disabled={!input.trim() && !selectedFile}
-                      onClick={sendPost}
-              
-                   >
-                     Post
-                   </button>
-                 </div>
+         </div>
+
+      <button className="
+         bg-[#1d9bf0] text-white 
+         transition duration-300 ease-in-out
+
+         
+         rounded-full px-4 py-1.5
+          font-bold shadow-md hover:bg-[#1a8cd8] 
+          disabled:hover:bg-[#1d9bf0] disabled:opacity-50 
+          disabled:cursor-default"
+          disabled={!input.trim() && !selectedFile}
+          onClick={sendPost}
+  
+       >
+         Post
+       </button>
+     </div>
+                 )}
+
+        
              </div>
             
         </div>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../components/sidebar/Sidebar';
 import Feed from '../components/feed/Feed';
+import { getProviders } from 'next-auth/react';
+
+
+import {getSession, getProviders,useSession} from 'next-auth/react';
+
+
 export default function Home() {
 
   return(
@@ -20,3 +26,26 @@ export default function Home() {
  
 }
 
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res)=> res.json()
+  );
+
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+
+
+  const providers = await getProviders(); //
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    }
+  }
+
+}
